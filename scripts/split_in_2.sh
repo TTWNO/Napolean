d94094347d48972b2fbe23c2e24970ecd6581bbb
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-05-02 19:10:45 +0000

    Add splitting image functionslity. Add .jpgs to .gitignore

---
+++ scripts/split_in_2.sh
@@ -0,0 +1,9 @@
+W=$(convert $1 -print "%w" /dev/null)
+H=$(convert $1 -print "%h" /dev/null)
+# Half width
+HW=$(($W / 2))
+
+# half-width wide, height tall, start at pixel 0
+convert $1 -crop ${HW}x${H}+0+0 left.jpg
+# half-wdith wide, height tall, start at half width
+convert $1 -crop ${HW}x${H}+${HW}+0 right.jpg
