6ab128f43da67ae89c606d9976ee6648a07e3854
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-05-02 20:51:22 +0100

    Add grayscale conversion

--- scripts/split_in_2.sh
+++ scripts/split_in_2.sh
@@ -4,6 +4,6 @@ H=$(convert $1 -print "%h" /dev/null)
 HW=$(($W / 2))
 
 # half-width wide, height tall, start at pixel 0
-convert $1 -crop ${HW}x${H}+0+0 left.jpg
+convert $1 -colorspace gray -crop ${HW}x${H}+0+0 left.jpg
 # half-wdith wide, height tall, start at half width
-convert $1 -crop ${HW}x${H}+${HW}+0 right.jpg
+convert $1 -colorspace gray -crop ${HW}x${H}+${HW}+0 right.jpg
