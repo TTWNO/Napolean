f1f3c34a36607dde7c509d9ceea5d2f2928e6e80
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-05-03 01:51:30 +0000

    Add sharpnes modifier to convert. Split using tesseract directly.

--- scripts/split_in_2.sh
+++ scripts/split_in_2.sh
@@ -2,8 +2,9 @@ W=$(convert $1 -print "%w" /dev/null)
 H=$(convert $1 -print "%h" /dev/null)
 # Half width
 HW=$(($W / 2))
+OPTIONS=(-colorspace gray -fill white -sharpen 0x2)
 
 # half-width wide, height tall, start at pixel 0
-convert $1 -colorspace gray -fill white -crop ${HW}x${H}+0+0 -normalize -threshold 35% left.jpg
+convert $1 "${OPTIONS[@]}" -crop ${HW}x${H}+0+0 left.jpg
 # half-wdith wide, height tall, start at half width
-convert $1 -colorspace gray -fill white -crop ${HW}x${H}+${HW}+0 -normalize -threshold 35% right.jpg
+convert $1 "${OPTIONS[@]}" -crop ${HW}x${H}+${HW}+0 right.jpg
