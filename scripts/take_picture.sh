6ea8f1c7dcae8df9034cd066b8a32760bb242a34
TTWNO <44244401+TTWNO@users.noreply.github.com>
2019-05-09 18:47:33 +0000

    Only print 'picture taken' when taking picture was successful

--- scripts/take_picture.sh
+++ scripts/take_picture.sh
@@ -3,6 +3,5 @@ STILL_OPTIONS=(-o $1)
 echo "Killing server!"
 kill $(ps -aux | grep StreamingCam.py | awk '/python/ {print $2}')
 echo "Taking picture!"
-raspistill "${STILL_OPTIONS[@]}"
-echo "Picture taken! Starting preview server."
+raspistill "${STILL_OPTIONS[@]}" && echo "Picture taken! Starting preview server."
 ./start_stream.sh
